1bb189fa3646ce0f0316b5ea7490d64b42dd1126
BIGZi0348 <svalker0348@gmail.com>
2026-03-07 14:16:29 +0300

    я передумал

--- Content.Server/Power/EntitySystems/ChargerSystem.cs
+++ Content.Server/Power/EntitySystems/ChargerSystem.cs
@@ -69,13 +69,13 @@ public sealed class ChargerSystem : SharedChargerSystem
                 {
                     // SS220 battery fix begin
                     // * В апстриме виздены полностью перелопатили батареи.
-                    // * Этот костыль при обновлении можно смело удалять,
-                    // * но не забыть поменять локали с chargePercentage на chargePercent
+                    // * Этот костыль при обновлении можно смело заменять,
                     if (!SearchForBattery(contained, out var _, out var battery))
                         continue;
+
+                    var chargePercent = (battery.CurrentCharge / battery.MaxCharge) * 100;
+                    args.PushMarkup(Loc.GetString("charger-content", ("chargePercent", (int)chargePercent)));
                     // SS220 battery fix end
-                    var chargePercentage = (battery.CurrentCharge / battery.MaxCharge) * 100;
-                    args.PushMarkup(Loc.GetString("charger-content", ("chargePercentage", (int)chargePercentage)));
                 }
             }
         }
